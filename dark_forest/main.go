4ebc4ab8557dccf2414db56b84e3bbbb245b71cb
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-05 15:31:53 +0300

    ЯндексÑ.Лицей. ДЗ. Темный лес

---
+++ dark_forest/main.go
@@ -0,0 +1,21 @@
+// ДЗ. Темный лес
+package main
+
+import "fmt"
+
+func main() {
+	var num int
+
+	fmt.Scanln(&num)
+
+	switch {
+	case num < 0 && num%2 == 0:
+		fmt.Println("Число отрицательное и четное")
+	case num < 0 && num%2 != 0:
+		fmt.Println("Число отрицательное и нечетное")
+	case num > 0 && num%2 == 0:
+		fmt.Println("Число положительное и четное")
+	case num > 0 && num%2 != 0:
+		fmt.Println("Число положительное и нечетное")
+	}
+}

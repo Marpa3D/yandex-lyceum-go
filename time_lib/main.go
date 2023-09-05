21957014ff69b77a7770659025ecd037d3d32991
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-05 12:27:23 +0300

    Пакет time

---
+++ time_lib/main.go
@@ -0,0 +1,12 @@
+// РАбота со временем
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println(now)
+}
--- user_input_scanln/main.go
+++ user_input_scanln/main.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	var age int
-	fmt.Println("Введите Ваш возраст:")
-	fmt.Scanln(&age)
+	fmt.Println("Введите ваш возраст:")
+	_, err := fmt.Scanln(&age)
+	if err != nil {
+		fmt.Printf("Ошибка %s", err)
+	}
 	fmt.Printf("Ваш возраст: %d лет.", age)
+	fmt.Printf("\nАдрес, где лежит считанное значение age: %p", &age)
 }

65f89cb4820361f6de5a67ee636bee9f4d32d625
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-06 15:17:04 +0300

    ДЗÑ. РекурсивыÐные функцииÑ. Факториал

---
+++ factorial/main.go
@@ -0,0 +1,15 @@
+// ДЗ. Рекурсия. Расчет факториала
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	fmt.Println(factorial(5))
+}

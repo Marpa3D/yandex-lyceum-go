5e70114006f9d10237cdf7a56b5cd29e2658545b
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-06 15:10:22 +0300

    Обновленный калькулятор с float64

--- func_calc/main.go
+++ func_calc/main.go
@@ -3,38 +3,42 @@ package main
 
 import "fmt"
 
-func sum(num1, num2 int) int {
-	return num1 + num2
-}
+//func sum(num1, num2 int) int {
+//	return num1 + num2
+//}
+//
+//func raz(num1, num2 int) int {
+//	return num1 - num2
+//}
+//
+//func mult(num1, num2 int) int {
+//	return num1 * num2
+//}
+//
+//func divide(num1, num2 int) (int, int) {
+//	integer := num1 / num2
+//	remainder := num1 % num2
+//
+//	return integer, remainder
+//}
 
-func raz(num1, num2 int) int {
-	return num1 - num2
-}
-
-func mult(num1, num2 int) int {
-	return num1 * num2
-}
-
-func divide(num1, num2 int) (int, int) {
-	integer := num1 / num2
-	remainder := num1 % num2
-
-	return integer, remainder
+func calc(n1, n2 float64) (float64, float64, float64, float64) {
+	return n1 + n2, n1 - n2, n1 * n2, n1 / n2
 }
 
 func main() {
-	var n1, n2 int
-
+	var n1, n2 float64
 	//fmt.Println("Добро пожаловать в калькулятор!")
 	fmt.Print("Введите первое число: ")
 	fmt.Scan(&n1)
 	fmt.Print("Введите второе число: ")
 	fmt.Scan(&n2)
 
-	resInt, resRemain := divide(n1, n2)
+	sum, raz, multip, divided := calc(n1, n2)
+	//resInt, resRemain := divide(n1, n2)
 
-	fmt.Println("Результат сложения: ", sum(n1, n2))
-	fmt.Printf("Результат вычитания: %d\n", raz(n1, n2))
-	fmt.Printf("Результат умножения: %d\n", mult(n1, n2))
-	fmt.Printf("Результат деления %d и остаток %d\n", resInt, resRemain)
+	fmt.Printf("Результат сложения: %0.1f\n", sum)
+	fmt.Printf("Результат вычитания: %0.1f\n", raz)
+	fmt.Printf("Результат умножения: %0.1f\n", multip)
+	fmt.Printf("Результат деления %0.1f\n", divided)
 }

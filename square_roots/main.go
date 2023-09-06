73a9848e39c6a5b2127334743b410534032050c6
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-06 20:31:28 +0300

    ДЗ. Квадратные корни

--- square_roots/main.go
+++ square_roots/main.go
@@ -1,14 +1,32 @@
 // ДЗ. Квадратные корни
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func sqRoots() {
-	var a, b, c float64
+func SqRoots() {
+	var a, b, c, discriminant, x1, x2 float64
 	fmt.Scanln(&a, &b, &c)
 
+	discriminant = b*b - 4*a*c
+
+	if discriminant > 0 {
+		x1 = (-b + math.Sqrt(discriminant)) / (2 * a)
+		x2 = (-b - math.Sqrt(discriminant)) / (2 * a)
+
+		fmt.Printf("%f %f", x2, x1)
+	}
+	if discriminant == 0 {
+		x1 = (-b + math.Sqrt(discriminant)) / (2 * a)
+		fmt.Printf("%f", x1)
+	}
+	if discriminant < 0 {
+		fmt.Println("0 0")
+	}
 }
 
 func main() {
-
+	SqRoots()
 }

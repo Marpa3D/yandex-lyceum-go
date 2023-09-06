dcb5071dc77ee42f4ad592eb4605339af9f54b17
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-06 20:39:18 +0300

    ДЗ. Функции. Функциональный калькулятор

---
+++ function_calc/main.go
@@ -0,0 +1,22 @@
+// ДЗ. Функции. Функциональный калькулятор
+package main
+
+import "fmt"
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func PrintNumbersAscending(n int) {
+	for i := 1; i <= n; i++ {
+		fmt.Printf("%d ", i)
+	}
+}
+
+func main() {
+	PrintNumbersAscending(10)
+}

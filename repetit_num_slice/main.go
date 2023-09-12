8060496d3a9fcf6250300087a1cd4123ab29b1d2
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-09-12 07:43:09 +0300

    Подсчет повторений в слайсе. ДЗÑ.

---
+++ repetit_num_slice/main.go
@@ -0,0 +1,29 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+// Подсчет повторений в слайсе
+import "fmt"
+
+// countOcc - функция подсчета повторений в слайсе
+func countOcc(nums []int) map[int]int {
+
+	res := make(map[int]int)
+
+	for _, num := range nums {
+		res[num]++
+	}
+	return res
+}
+
+func main() {
+	n := []int{1, 3, 4, 1, 8, 3, 4, 8, 8}
+
+	result := countOcc(n)
+
+	fmt.Println("Результат подсчета повторений:")
+
+	for num, count := range result {
+		fmt.Printf("%d встерачается %d раза\n", num, count)
+	}
+}
